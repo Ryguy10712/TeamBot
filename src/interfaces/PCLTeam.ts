7fa64c6d7c7f2344856dbb78e36ac3a3e07b53cd
Ryguy10712 <ryguy10712@outlook.com>
2022-11-15 22:11:50 -0500

    added properties "name" and "confidential"

--- src/interfaces/PCLTeam.ts
+++ src/interfaces/PCLTeam.ts
@@ -6,9 +6,11 @@ export enum Ranks {
 }
 
 export interface PCLTeam {
+    name: string
     players: PCLPlayer[];
     rank: Ranks | undefined
     guildID: string | undefined,
+    confidential: boolean
     isWeeklySchedulingPollsEnabled: boolean | undefined,
     captain: PCLPlayer,
     coCap: PCLPlayer | undefined
