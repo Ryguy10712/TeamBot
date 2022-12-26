7dd33a161ed0b9e028ceba8771d098faa7b0a85f
Ryguy10712 <ryguy10712@outlook.com>
2022-12-25 21:37:43 -0500

    stopped tracking and removed dist folder

--- dist/discord/DiscordButton.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DiscordButton = void 0;
-const builders_1 = require("@discordjs/builders");
-class DiscordButton extends builders_1.ButtonBuilder {
-}
-exports.DiscordButton = DiscordButton;
-//# sourceMappingURL=DiscordButton.js.map
--- dist/events/ScheduleChannelSet.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=ScheduleChannelSet.js.map
