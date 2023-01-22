69761149203badc54494d8cd3469d17ae3bc7379
Ryguy10712 <ryguy10712@outlook.com>
2023-01-21 21:42:41 -0500

    forgor to do the schedule request deny button 💀

--- src/discord/buttons/ScheduleRequestDeny.ts
+++ src/discord/buttons/ScheduleRequestDeny.ts
@@ -1,4 +1,4 @@
-import { Client, ButtonInteraction, CacheType, DMChannel, ButtonStyle } from "discord.js";
+import { Client, ButtonInteraction, CacheType, ButtonStyle } from "discord.js";
 import { DiscordButton } from "../DiscordButton";
 import fs from "fs";
 import { ScheduleRequest } from "../../interfaces/ScheduleRequest";
@@ -11,30 +11,42 @@ export class ScheduleRequestDenyButton extends DiscordButton {
     constructor() {
         super();
         this.id = "schedreqDeny";
-        this.setLabel("Decline")
-        this.setStyle(ButtonStyle.Danger)
-        this.setCustomId(this.id)
+        this.setLabel("Decline");
+        this.setStyle(ButtonStyle.Danger);
+        this.setCustomId(this.id);
     }
 
     async execute(teamBot: TeamBot, client: Client<boolean>, interaction: ButtonInteraction<CacheType>) {
         const schedReqDb: ScheduleRequest[] = JSON.parse(fs.readFileSync("./db/scheduleRequests.json", "utf-8"));
-        const teamsDb: PCLTeam[] = JSON.parse(fs.readFileSync("./db/teams.json", "utf-8"))
+        const teamsDb: PCLTeam[] = JSON.parse(fs.readFileSync("./db/teams.json", "utf-8"));
 
-        const schedReq = schedReqDb.find((schedReq) => {
-            return schedReq.captainMsgId === interaction.message.id || schedReq.coCaptainMsgId === interaction.message.id;
-        })!;
+        const schedReq = await teamBot.prisma.scheduleRequest.findFirst({
+            where: { OR: [{ captainMsgId: interaction.message.id }, { coCaptainMsgId: interaction.message.id }] },
+            include: {
+                requesterTeam: {
+                    select: {
+                        players: {
+                            where: { OR: [{ isCaptain: true }, { isCoCap: true }] },
+                            select: { playerId: true },
+                        },
+                    },
+                },
+                receiverTeam: {
+                    select: { name: true },
+                },
+            },
+        });
 
-        schedReqDb.splice(schedReqDb.indexOf(schedReq), 1)
-        fs.writeFileSync("./db/scheduleRequests.json", JSON.stringify(schedReqDb))
+        teamBot.prisma.scheduleRequest
+            .delete({
+                where: { id: schedReq?.id },
+            })
+            .then(() => {
+                teamBot.prisma.$disconnect();
+            });
 
-        const requesterTeam: PCLTeam = teamsDb.find(pclTeam => {return pclTeam.name === schedReq.requester})!
-        const declinerTeam: PCLTeam = teamsDb.find(pclTeam => {return pclTeam.name === schedReq.opponent})!
-
-        const requestChannel = await client.channels.fetch(schedReq.requestChanId) as DMChannel
-        const requestMsg = await requestChannel.messages.fetch(schedReq.captainMsgId)
-
-        requestMsg.reply("this has been denied")
-
-        
+        teamBot.client.users.send(schedReq?.requesterTeam.players[0].playerId!, `The schedule request against ${schedReq?.receiverTeam.name} has been denied`);
+        teamBot.client.users.send(schedReq?.requesterTeam.players[1].playerId!, `The schedule request against ${schedReq?.receiverTeam.name} has been denied`);
     }
+    
 }
