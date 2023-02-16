575d5c2a3558c41020ba8086c16befddc59ac43e
Ryguy10712 <ryguy10712@outlook.com>
2023-02-15 19:06:29 -0500

    e

--- src/discord/commands/RefreshAvailability.ts
+++ src/discord/commands/RefreshAvailability.ts
@@ -33,7 +33,7 @@ export class RefreshAvailabilityCommand extends DiscordCommand {
 
     private async filterTeamUsers(users: IterableIterator<User>, teamPlayers: TeamPlayer[]) {
         const filteredPlayers: TeamPlayer[] = []
-        for await (const user of users){
+        for (const user of users){
             const teamPlayer = teamPlayers.find(player => {
                 return player.playerId == user.id
             })
@@ -47,33 +47,35 @@ export class RefreshAvailabilityCommand extends DiscordCommand {
     private async fixReactionInfo(reaction: MessageReaction, filteredUsers: TeamPlayer[], prisma: PrismaClient) {
         const timeOfEmoji = this.reactionToTime[reaction.emoji.name!]
         const day = reaction.message.content?.toLowerCase() as DayOfWeek
-        for await (const user of filteredUsers) {
+        for (const user of filteredUsers) {
             let dayInfo = user[day] ? user[day] as availability : {}
             if(!dayInfo[timeOfEmoji]){
                 dayInfo[timeOfEmoji] = true
-                await prisma.teamPlayer.update({
+                prisma.teamPlayer.update({
                     where: {playerId: user.playerId},
                     data: {[day]: dayInfo}
-                })
-                prisma.$disconnect()
+                }).then(() => {prisma.$disconnect()})
             }
         }
     }
 
     async executeInteraction(client: Client<boolean>, interaction: CommandInteraction<CacheType>, teamBot: TeamBot) {
+        if(!teamBot.isAdmin(interaction.user.id)) {
+            interaction.reply({ephemeral: true, content: "This command is not ready yet"})
+            return;
+        }
         interaction.deferReply({ephemeral: true})
         const daysOfWeek: DayOfWeek[] = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-        const validReactions = ["1️⃣" , "2️⃣" , "3️⃣" , "4️⃣" , "5️⃣" , "6️⃣" , "7️⃣" , "8️⃣" , "9️⃣" , "🔟" , "🕚" , "🕛"]
         const issuer = await teamBot.prisma.teamPlayer.findFirst({
             where: { playerId: interaction.user.id, OR: [{ isCaptain: true }, { isCoCap: true }] },
             include: { team: { include: { availability: true, players: true }, } },
         });
         if (!issuer) {
-            interaction.reply({ embeds: [new UserNotCaptainOrEmbed()], ephemeral: true });
+            interaction.followUp({ embeds: [new UserNotCaptainOrEmbed()], ephemeral: true });
             return;
         }
         if (!issuer.team.schedulingChannel) {
-            interaction.reply({ content: "You must have a scheduling channel to run this command", ephemeral: true });
+            interaction.followUp({ content: "You must have a scheduling channel to run this command", ephemeral: true });
             return;
         }
 
@@ -84,7 +86,7 @@ export class RefreshAvailabilityCommand extends DiscordCommand {
             const msg = await schedulingChannel.messages.fetch(availability[day]);
             const reactions = msg.reactions.valueOf().values()
             
-            for await(const reaction of reactions){
+            for (const reaction of reactions){
                 const users = (await reaction.users.fetch()).values()
                 const filteredUsers = await this.filterTeamUsers(users, issuer.team.players)
                 this.fixReactionInfo(reaction, filteredUsers, teamBot.prisma);
